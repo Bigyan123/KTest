5b5aeb6353803d3794edbff42e1e75cab8c1e309
Bingyan Sahoo <bingyan.sahoo@intimetec.com>
2020-04-24 09:43:03 +0530

    changes

---
+++ src/main/java/com/gitlab/KTest/KExample.java
@@ -0,0 +1,14 @@
+package com.gitlab.KTest;
+
+public class KExample {
+
+	public String GetData(String x, String y) {
+		return x+y;
+	}
+	
+	public int GetiData(int x, int y) {
+		return x+y;
+	}
+	
+	
+}
